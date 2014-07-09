fa7ac3f651910b36aaffe09806aae7ebd70ef5c9
danzel <danzel@localhost.geek.nz>
2014-07-10 11:59:58 +1200

    Add a GeoJsonDataSource

---
+++ TileSharp.Data.GeoJson/GeoJsonDataSource.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using GeoAPI.Geometries;
+using GeoJsonSharp;
+
+namespace TileSharp.Data.GeoJson
+{
+	public class GeoJsonDataSource : IDataSource
+	{
+		private readonly FeatureCollection _featureCollection;
+
+		public GeoJsonDataSource(string fileName)
+		{
+			_featureCollection = new GeoJsonParser(File.ReadAllText(fileName), new ParserSettings { SkipInvalidGeometry = true }).Parse();
+		}
+
+		public List<IGeometry> Fetch(Envelope envelope)
+		{
+			var res = new List<IGeometry>();
+
+			foreach (var feature in _featureCollection.Features)
+			{
+				if (feature.Geometry.EnvelopeInternal.Intersects(envelope))
+				{
+					//Convert Multi-X to X
+					if (feature.Geometry is IMultiLineString || feature.Geometry is IMultiPolygon)
+					{
+						for (var i = 0; i < feature.Geometry.NumGeometries; i++)
+							res.Add(feature.Geometry.GetGeometryN(i));
+					}
+					else
+					{
+						res.Add(feature.Geometry);
+					}
+				}
+			}
+
+			return res;
+		}
+	}
+}
